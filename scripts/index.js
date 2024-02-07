5811dc3af14588959df6fa5701ec43610a233220
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2024-02-07 08:45:47 +0800

    Modify the style.css, index.html and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 navlinks = document.querySelector('.navlinks')
 hamburgerMenu = document.querySelector('.hamburger-menu')
+crossBtn = document.querySelector('.cross-button')
+
 
 const menuShow = () =>{
     navlinks.classList.add('show')
+}
+
+const closeMenu = () =>{
+    navlinks.classList.remove('show')
 }
